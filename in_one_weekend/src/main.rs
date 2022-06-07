a4de67b1970dc45ee28407546464a7ef73b4a893
Collin Prince <collin@Collins-MBP-2.lan>
2022-06-07 15:16:16 -0700

    update app to begin displaying spheres and shading them

--- in_one_weekend/src/main.rs
+++ in_one_weekend/src/main.rs
@@ -4,13 +4,30 @@ mod vec3;
 
 use color::write_color;
 use ray::Ray;
-use vec3::{unit_vector, Color, Point3, Vec3};
+use vec3::{dot, unit_vector, Color, Point3, Vec3};
 
 use std::io::{self, Write};
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
+    let oc: Vec3 = r.origin() - *center;
+    let a: f64 = r.direction().length_squared();
+    let half_b: f64 = dot(&oc, &r.direction());
+    let c: f64 = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+    match discriminant < 0.0 {
+        true => -1.0,
+        false => (-half_b - discriminant.sqrt()) / a,
+    }
+}
+
 fn ray_color(r: &Ray) -> Color {
+    let mut t: f64 = hit_sphere(&Point3::new().set_values(0.0, 0.0, -1.0), 0.5, &r);
+    if t > 0.0 {
+        let n: Vec3 = unit_vector(r.at(t) - Vec3::new().set_values(0.0, 0.0, -1.0));
+        return 0.5 * Color::new().set_values(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
+    }
     let unit_dir: Vec3 = unit_vector(r.direction());
-    let t: f64 = 0.5 * (unit_dir.y() + 1.0);
+    t = 0.5 * (unit_dir.y() + 1.0);
     (1.0 - t) * Color::new().set_values(1.0, 1.0, 1.0) + t * Color::new().set_values(0.5, 0.7, 1.0)
 }
 
