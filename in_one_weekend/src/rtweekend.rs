f3b4420355ca54af33afd5a66945aa6857232a26
Collin Prince <cjp915@gmail.com>
2022-06-07 17:50:55 -0700

    created hittable trait and added sphere class to abstract creating spheres in images

---
+++ in_one_weekend/src/rtweekend.rs
@@ -0,0 +1,8 @@
+// constants
+pub const INFINITY: f64 = std::f64::INFINITY;
+pub const PI: f64 = 3.1415926535897932385;
+
+// utility functions
+pub fn degrees_to_radians(degrees: f64) -> f64 {
+    degrees * PI / 180.0
+}
