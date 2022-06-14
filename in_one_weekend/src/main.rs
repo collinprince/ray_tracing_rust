a7d8587b13a7235a6aec598384a81ce40f530850
Collin Prince <cjp915@gmail.com>
2022-06-14 15:44:37 -0700

    add glass surfaces

--- in_one_weekend/src/main.rs
+++ in_one_weekend/src/main.rs
@@ -53,9 +53,9 @@ fn main() {
     let mut world: HittableList = HittableList::new();
 
     let material_ground: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let material_left: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
-    let material_right: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+    let material_center: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left: Rc<dyn Material> = Rc::new(Dielectric::new(1.5));
+    let material_right: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     world.add(Box::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
@@ -70,6 +70,11 @@ fn main() {
     world.add(Box::new(Sphere::new(
         Point3::new(-1.0, 0.0, -1.0),
         0.5,
+        Rc::clone(&material_left),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.4,
         Rc::clone(&material_left),
     )));
     world.add(Box::new(Sphere::new(
--- in_one_weekend/src/material.rs
+++ in_one_weekend/src/material.rs
@@ -1,7 +1,8 @@
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
+use crate::rtweekend::random_f64;
 use crate::vec3::{
-    dot, random_in_unit_sphere, random_unit_vector, reflect, unit_vector, Color, Vec3,
+    dot, random_in_unit_sphere, random_unit_vector, reflect, refract, unit_vector, Color, Vec3,
 };
 
 pub struct Output {
@@ -64,5 +65,52 @@ impl Material for Metal {
         } else {
             None
         }
+    }
+}
+
+pub struct Dielectric {
+    pub ir: f64,
+}
+
+impl Dielectric {
+    pub fn new(index_of_refraction: f64) -> Dielectric {
+        Dielectric {
+            ir: index_of_refraction,
+        }
+    }
+
+    fn reflectance(&self, cosine: f64, ref_idx: f64) -> f64 {
+        // use schlick's approximation for reflectance
+        let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<Output> {
+        let attenuation: Color = Color::new(1.0, 1.0, 1.0);
+        let refraction_ratio: f64 = if rec.front_face {
+            1.0 / self.ir
+        } else {
+            self.ir
+        };
+
+        let unit_direction: Vec3 = unit_vector(r_in.direction());
+        let cos_theta: f64 = dot(&-unit_direction, &rec.normal).min(1.0);
+        let sin_theta: f64 = (1.0 - cos_theta.powi(2)).sqrt();
+
+        let cannot_refract: bool = refraction_ratio * sin_theta > 1.0;
+
+        let schlick_approx: bool = self.reflectance(cos_theta, refraction_ratio) > random_f64();
+        let direction: Vec3 = if cannot_refract || schlick_approx {
+            reflect(unit_direction, rec.normal)
+        } else {
+            refract(unit_direction, rec.normal, refraction_ratio)
+        };
+        let scattered: Ray = Ray::new(rec.p, direction);
+        Some(Output {
+            attenuation,
+            scattered,
+        })
     }
 }
